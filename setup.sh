d533024cd0cd338f7df074b0863c5b9f71036939
Yevhenii Huselietov <yevhenii.huselietov@shopify.com>
2021-05-04 01:57:29 +0000

    Use home directory

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ sudo ln -nfs $DIR/scripts/git-squash /usr/local/bin/git-squash
 cp -R $DIR/.config/fish $HOME/.config/fish
 cp gitconfig ~/.gitconfig
 
-cp fonts/Consolasligaturizedv2.ttf /usr/local/share/fonts/
+mkdir -p ~/.fonts
+cp fonts/Consolasligaturizedv2.ttf ~/.fonts/
