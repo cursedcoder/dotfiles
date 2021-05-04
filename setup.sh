eda4d43db9ad83a6bfe5e8abd71cb7aecb1ec020
Yevhenii Huselietov <yevhenii.huselietov@shopify.com>
2021-05-04 01:56:23 +0000

    Add consolas font for personal use

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ sudo ln -nfs $DIR/scripts/git-squash /usr/local/bin/git-squash
 
 cp -R $DIR/.config/fish $HOME/.config/fish
 cp gitconfig ~/.gitconfig
+
+cp fonts/Consolasligaturizedv2.ttf /usr/local/share/fonts/
